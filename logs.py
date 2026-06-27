14d68d176b58d5cba6b186ee04a78daaf65e7917
newkrishna983-lang <newkrishna983@gmail.com>
2026-06-27 10:28:16 +0530

    Add files via upload

---
+++ logs.py
@@ -0,0 +1,20 @@
+# Don't Remove Credit Tg - @newstudent1885
+# Ask Doubt on telegram @newstudent1885
+
+import logging
+from logging.handlers import RotatingFileHandler
+
+logging.basicConfig(
+    level=logging.ERROR,
+    format=
+    "%(asctime)s - %(levelname)s - %(message)s [%(filename)s:%(lineno)d]",
+    datefmt="%d-%b-%y %H:%M:%S",
+    handlers=[
+        RotatingFileHandler("logs.txt", maxBytes=50000000, backupCount=10),
+        logging.StreamHandler(),
+    ],
+)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+logging = logging.getLogger()
---
+++ vars.py
@@ -0,0 +1,8 @@
+# Don't Remove Credit Tg - @newstudent1885
+# Ask Doubt on telegram @newstudent1885
+
+from os import environ
+
+API_ID = int(environ.get("API_ID", "2447366")) #Replace with your api id
+API_HASH = environ.get("API_HASH", "5dghgfccc") #Replace with your api hash
+BOT_TOKEN = environ.get("BOT_TOKEN", "7231089830:A") #Replace with your bot token
