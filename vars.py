4fe1ac2548f9c5abf1030f292584e380839645ae
newkrishna983-lang <newkrishna983@gmail.com>
2026-06-27 10:46:43 +0530

    Remove owner and admin configuration
    Removed owner and admin configuration variables from vars.py.

--- vars.py
+++ vars.py
@@ -6,6 +6,3 @@ from os import environ
 API_ID = int(environ.get("API_ID", "34422904")) #Replace with your api id
 API_HASH = environ.get("API_HASH", "7e0002469784f47fc08a6b3d93d7ebed") #Replace with your api hash
 BOT_TOKEN = environ.get("BOT_TOKEN", "8152006541:AAHxqnqOVSgQcFjsx5aWjFF_m6oabDutVew") #Replace with your bot token
-# Owner and Admin Configuration
-OWNER_ID = int(os.environ.get("OWNER_ID", "5349573682"))
-ADMINS = [int(x) for x in os.environ.get("ADMINS", "").split()]  # Default to owner ID
